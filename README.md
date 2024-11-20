4e934d7efad97eaa5f52090795cc1e15ceb5ff14
NOUSHEEN ATIF <161060487+codewithhoney24@users.noreply.github.com>
2024-11-20 01:45:06 -0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Portfolio" 
+"# web-port-custom" 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,33 +1,25 @@
-import React from 'react'
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 import "../app/styles/hero.css";
 
-
-const Hero =() =>  {
+const Hero = () => {
   return (
-    <div id="hero"
-     className='hero-container'>
-      <div className='hero-content'>
-          <div className='hidden lg:block'></div>
-          <div className='hero-text'>
-            <div className='hero-msin'>
-            <div className="N-img">
-  <Image 
-    src="/gray__1_-removebg-preview.png"
-    alt="N image"
-    width={300}  // Set width for a smaller size
-    height={300} // Set height proportionally
-    style={{ objectFit: "cover" }}
-  />
-</div>
+    <div id="hero" className="hero-container">
+      <div className="N-img">
+        <Image
+          src="/gray__1_-removebg-preview.png"
+          alt="N image"
+          width={300} // Set width for a smaller size
+          height={300} // Set height proportionally
+          style={{ objectFit: "cover" }}
+        />
+      </div>
 
-       
-              <p data-aos="zoom-in-up">I am</p>
-              <p data-aos="zoom-in-up">Nousheen</p>
-              <p data-aos="zoom-in-up">Atif</p>
-            </div>
-          </div>
-        </div>
+      <div className="para-text">
+        <p>I am</p>
+        <p>Nousheen</p>
+        <p>Atif</p>
+      </div>
     </div>
   );
 };
